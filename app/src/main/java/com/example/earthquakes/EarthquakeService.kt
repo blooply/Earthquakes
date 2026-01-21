842dee732f4d14fec22afe2b3932eb6ecd03b684
Daxton Brown <nugnuts.the.2nd@gmail.com>
2026-01-21 09:12:22 -0800

    Start EarthquakeService.kt

---
+++ app/src/main/java/com/example/earthquakes/EarthquakeService.kt
@@ -0,0 +1,13 @@
+package com.example.earthquakes
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface EarthquakeService {
+    // where you list out the different endpoints in the API you want to call
+    // function returns Call<type> where type is the data returned in the json
+    // in the @GET("blah"), "blah" is the path to the file (endpoint)
+
+    @GET("summary/significant_day.geojson")
+    fun getEarthquakeDataPastDay() : Call<FeatureCollection>
+}
