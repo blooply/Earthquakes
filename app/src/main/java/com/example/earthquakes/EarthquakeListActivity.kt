0d87fe85e32b64abe1242b7c4b2648d8a795a151
Daxton Brown <nugnuts.the.2nd@gmail.com>
2026-01-21 09:37:34 -0800

    Start callback stuff

--- app/src/main/java/com/example/earthquakes/EarthquakeListActivity.kt
+++ app/src/main/java/com/example/earthquakes/EarthquakeListActivity.kt
@@ -1,10 +1,16 @@
 package com.example.earthquakes
 
+import RetrofitHelper
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.create
 
 class EarthquakeListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +22,28 @@ class EarthquakeListActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val earthquakeService = RetrofitHelper.getInstance().create(EarthquakeService::class.java)
+        val earthquakeCall = earthquakeService.getEarthquakeDataPastDay()
+        earthquakeCall.enqueue(object: Callback<FeatureCollection> {
+            override fun onResponse(
+                call: Call<FeatureCollection?>,
+                response: Response<FeatureCollection?>
+            ) {
+                val featureCollection = response.body()
+                // check if the underlying array isn't null, then put it into your recyclerview adapter
+                // anything that expects you to have featureCollection data has to be here
+            }
+
+            override fun onFailure(
+                call: Call<FeatureCollection?>,
+                t: Throwable
+            ) {
+                // log your failure error message
+                Log.d("EarthquakeList", "onFailure: ${t.message}")
+            }
+        } )
+
+        // if you try to use featureCollection data out here, it might not exist
     }
 }
