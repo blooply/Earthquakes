3a31d62cb498b8ce1b737a44343b26bb48b20ce5
Daxton Brown <nugnuts.the.2nd@gmail.com>
2026-01-27 10:42:54 -0800

    Refactor activity_main.xml to activity_earthquake_list.xml

--- app/src/main/java/com/example/earthquakes/EarthquakeListActivity.kt
+++ app/src/main/java/com/example/earthquakes/EarthquakeListActivity.kt
@@ -10,7 +10,6 @@ import androidx.core.view.WindowInsetsCompat
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.create
 
 class EarthquakeListActivity : AppCompatActivity() {
 
@@ -21,7 +20,7 @@ class EarthquakeListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_earthquake_list)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
